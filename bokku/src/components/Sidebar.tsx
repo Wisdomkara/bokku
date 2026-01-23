961748e7e1d51b32a00275a48466a3e5ec598787
Wisdom Kara <wiskara1@gmail.com>
2026-01-23 14:46:37 +0100

    i wanna add a sidebar

---
+++ bokku/src/components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+const Sidebar = () => {
+    const [categories, Setcategories] = useState<string[]>([]);
+    const [keywords] = useState([
+
+        "bread",
+        "drinks",
+        "home care",
+        "personal hygiene",
+        "personal hygiene",
+    ])
+};
+
+export default Sidebar;
